5cfa178de69cce2a7bddf3f841bb2caa9c638e33
Anders Jaensson <anders.jaensson@smartbear.com>
2021-01-21 10:11:48 +0100

    need to commit

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -15,23 +15,23 @@ public class ShoutSteps {
     private static final String ARBITRARY_MESSAGE = "Hello, world";
     private final Shouty shouty = new Shouty();
 
-    @Given("Lucy is at {int}, {int}")
-    public void lucy_is_at(int xCoord, int yCoord) {
-        shouty.setLocation("Lucy", new Coordinate(xCoord, yCoord));
+    @Given("{word} is at {int}, {int}")
+    public void person_is_at(String name, int xCoord, int yCoord) {
+        shouty.setLocation(name, new Coordinate(xCoord, yCoord));
     }
 
-    @Given("Sean is at {int}, {int}")
-    public void sean_is_at(int xCoord, int yCoord) {
-        shouty.setLocation("Sean", new Coordinate(xCoord, yCoord));
-    }
-
-    @When("Sean shouts")
-    public void sean_shouts() {
-        shouty.shout("Sean", ARBITRARY_MESSAGE);
+    @When("{word} shouts")
+    public void person_shouts(String name) {
+        shouty.shout(name, ARBITRARY_MESSAGE);
     }
 
     @Then("Lucy should hear Sean")
     public void lucy_should_hear_sean() {
+        assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
+    }
+
+    @Then("Lucy should not hear Oscar")
+    public void lucy_should_not_hear_oscar() {
         assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
     }
 
