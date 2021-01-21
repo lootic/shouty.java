b7ee6283b7e6c90d5609688b37e66d3bea1c692f
Joel Jonsson <joel.jonsson@smartbear.com>
2021-01-21 11:43:53 +0100

    page 10

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -4,9 +4,13 @@ import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import java.util.List;
+import java.util.Map;
 
 
 public class ShoutSteps {
@@ -36,5 +40,33 @@ public class ShoutSteps {
     @Then("{word} should not hear {word}")
     public void listenerShouldNotHearShouter(String listener, String shouter) {
         assertNull(shouty.getShoutsHeardBy(listener).get(shouter));
+    }
+
+    @Given("people are located at")
+    public void peopleAreLocatedAt(List<PersonLocation> personLocations) {
+        for (PersonLocation personLocation : personLocations) {
+            shouty.setLocation(personLocation.name, new Coordinate(personLocation.x, personLocation.y));
+        }
+    }
+
+    private static class PersonLocation {
+        String name;
+        int x, y;
+
+        public PersonLocation(String name, int x, int y) {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @DataTableType
+    public PersonLocation definePersonLocation(Map<String, String> entry) {
+        return new PersonLocation(
+                entry.get("name"),
+                Integer.parseInt(entry.get("x")),
+                Integer.parseInt(entry.get("y"))
+        );
     }
 }
